320c87e48969a4e69b736db2a06043e99da80a2e
Uon Liaquat <antelopes06@gmail.com>
2021-06-03 17:30:38 +0500

    Setup Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App.js';
+import { ApolloProvider } from '@apollo/react-hooks'
 import reportWebVitals from './reportWebVitals';
+import client from './client.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Root = () => (
+ <React.StrictMode>
+      <ApolloProvider client={client}> 
+          <App />
+      </ApolloProvider>
+  </React.StrictMode>
 );
 
+ReactDOM.render(<Root />, document.getElementById('root'))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
