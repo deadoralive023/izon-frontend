5883a2add5d1f52d0c003dd0a37226650b922a7a
Hamza Noor <hamza.randhawa@deltalabs.io>
2021-06-07 22:06:44 +0500

    Add Routes

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,66 @@ import { Navbar } from '../components/Navbar/index.js'
 
 import { Footer } from '../components/Footer/index.js'
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
+
+
+//   return (
+//     <Router>
+//       <div>
+
+//         {/* A <Switch> looks through its children <Route>s and
+//             renders the first one that matches the current URL. */}
+//         <Switch>
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route path="/users">
+//             <Users />
+//           </Route>
+//           <Route path="/">
+//             <Home />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
 
 
 export const App = () => {
     return (
+        
         <div className="App">
-            <Navbar />
+        <Navbar />
+        
+        <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+                <Route path="/" exact>
+                    <Products />
+                </Route>
+                <Route path="/categories" exact>
+                    <Categories />
+                </Route>
+            </Switch>
+        </Router>
+
+
+            {/* <Navbar />
         
             <Products />
-            {/* <Categories /> */}
+            <Categories />
+
+            <Footer /> */}
 
             <Footer />
-
+            
         </div>
     )
 }
