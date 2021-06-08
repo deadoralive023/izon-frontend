ce4090c685cb34db7654ec2709ce79d293875e7b
Uon Liaquat <antelopes06@gmail.com>
2021-06-08 07:46:35 +0500

    Added Cart and CartItem Components

---
+++ src/components/Cart/CartItem/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Typography, Button, Card, CardActions, CardContent, CardMedia} from '@material-ui/core'
+import useStyles from './styles'
+
+export default Cart = ({item}) => {
+    const classes = useStyles()
+    return(
+        <Card>
+            <CardMedia className={classes.media} image={item.imageurl} alt={item.name} />
+            <CardContent className={classes.cardContent}>
+                <Typography variant="h4">{item.name}</Typography>
+                <Typography variant="h5">{item.line_total}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <div className={classses.buttons}>
+                    <Button type="button' size="small">-</Button>
+                    <Typography>{item.quantity}</Typography>
+                    <Button type="button' size="small">+</Button>
+
+                </div>
+            <CardActions/>
+        </Card>
+
+    )
+}
